070b7cd609c052e8a0b404e3afe0a23752f4fd75
nasir21b@gmail.com <nasir21b@gmail.com>
2026-05-06 23:20:05 +0600

    Problem 1 Solved filterEvenNumbers

---
+++ solutions.ts
@@ -0,0 +1,9 @@
+// Problem 1:
+
+const filterEvenNumbers = (arrOfNumbers: number[]): number[] => {
+  const evenNumbers = arrOfNumbers.filter((num) => num % 2 == 0);
+  return evenNumbers;
+};
+
+const result = filterEvenNumbers([1, 2, 3, 4, 5, 6, 12, 45, 50, 11]);
+console.log("Problem 1 Output:", result);
