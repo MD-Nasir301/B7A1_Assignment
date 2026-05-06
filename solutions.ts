085c52f43634d2b51e75c815cc45c2e2a053fe38
nasir21b@gmail.com <nasir21b@gmail.com>
2026-05-07 00:09:27 +0600

    Problem 2 Solved: Revers String

--- solutions.ts
+++ solutions.ts
@@ -7,3 +7,13 @@ const filterEvenNumbers = (arrOfNumbers: number[]): number[] => {
 
 const result = filterEvenNumbers([1, 2, 3, 4, 5, 6, 12, 45, 50, 11]);
 console.log("Problem 1 Output:", result);
+
+// Problem 2:
+
+const reverseString = (inputText: string): string => {
+  const reversText = inputText.split("").reverse().join("");
+  return reversText;
+};
+
+const result2 = reverseString("Nasir");
+console.log("Problem 2 Output:", result2);
